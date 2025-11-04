296967f414e89b70f6cf1930f907dbf9e86837b0
janiocarlos <janiocarlosns@gmail.com>
2025-11-04 11:08:52 -0300

    creation menu pt.2

--- app/src/main/java/com/example/ondecomprar/MainActivity.java
+++ app/src/main/java/com/example/ondecomprar/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.ondecomprar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        menu = findViewById(R.id.bottom_nav);
+
+        menu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if(menuItem.getItemId() == R.id.action_profile){
+                    startActivity(new Intent(MainActivity.this, TelaUsuario.class));
+                }
+                return true;
+            }
         });
     }
 }
